46aee46e19d5009f57e02ac5bb5603087ddd9941
Aneek Mubarak <aneek.mubarak@gmail.com>
2025-07-10 20:46:48 -0600

    created weight mem 1 and 2 and filled with weights

--- quartus_project/mnist_nn.sv
+++ quartus_project/mnist_nn.sv
@@ -1,6 +1,9 @@
 module mnist_nn (
 	input clk,
-	input reset
+	input reset,
+	output reg [9:0] inp_count,
+	output reg [9:0] inp_count_d,
+	output wire [15:0] weight_value_0, weight_value_1
 
 );
 
@@ -10,14 +13,39 @@ module mnist_nn (
 
 
 
-wire signed [7:0] weight_value;
+//wire signed [7:0] weight_value_0, weight_value_1, weight_value_2, weight_value_3, weight_value_4, weight_value_5, weight_value_6, weight_value_7, weight_value_8, weight_value_9;
+  
+//reg [9:0] inp_count;
 
-reg [9:0] inp_count;
+always @ (posedge clk)
+if (reset) 
+	inp_count <= 10'b0;
+else if (inp_count == 10'd783) //reset in next cycle
+	inp_count <= 10'd0;
+else
+	inp_count <= inp_count + 1'b1;
 
-weight_memory weight_mem_0(.address(inp_count),
-										.clock(clk),
-										.q(weight_value) 
+	
+always @ (posedge clk)
+	inp_count_d <= inp_count;
+
+weight_mem0 neuron0_weights(.address(inp_count),
+										.clock(~clk),
+										.q(weight_value_0) 
 										);
+										
+weight_mem1 neuron1_weights(.address(inp_count),
+										.clock(~clk),
+										.q(weight_value_1) 
+										);
+										
+
+
+										
+
+
+										
+
 										
 
 										
--- quartus_project/weight_mem2.v
+++ quartus_project/weight_mem2.v
@@ -1,10 +1,10 @@
-// megafunction wizard: %ROM: 1-PORT%VBB%
+// megafunction wizard: %ROM: 1-PORT%
 // GENERATION: STANDARD
 // VERSION: WM1.0
 // MODULE: altsyncram 
 
 // ============================================================
-// File Name: weight_memory.v
+// File Name: weight_mem2.v
 // Megafunction Name(s):
 // 			altsyncram
 //
@@ -16,6 +16,7 @@
 //
 // 23.1std.1 Build 993 05/14/2024 SC Lite Edition
 // ************************************************************
+
 
 //Copyright (C) 2024  Intel Corporation. All rights reserved.
 //Your use of Intel Corporation's design tools, logic functions 
@@ -32,14 +33,18 @@
 //refer to the applicable agreement for further details, at
 //https://fpgasoftware.intel.com/eula.
 
-module weight_memory (
+
+// synopsys translate_off
+`timescale 1 ps / 1 ps
+// synopsys translate_on
+module weight_mem2 (
 	address,
 	clock,
 	q);
 
 	input	[9:0]  address;
 	input	  clock;
-	output	[7:0]  q;
+	output	[15:0]  q;
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_off
 `endif
@@ -47,6 +52,50 @@ module weight_memory (
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_on
 `endif
+
+	wire [15:0] sub_wire0;
+	wire [15:0] q = sub_wire0[15:0];
+
+	altsyncram	altsyncram_component (
+				.address_a (address),
+				.clock0 (clock),
+				.q_a (sub_wire0),
+				.aclr0 (1'b0),
+				.aclr1 (1'b0),
+				.address_b (1'b1),
+				.addressstall_a (1'b0),
+				.addressstall_b (1'b0),
+				.byteena_a (1'b1),
+				.byteena_b (1'b1),
+				.clock1 (1'b1),
+				.clocken0 (1'b1),
+				.clocken1 (1'b1),
+				.clocken2 (1'b1),
+				.clocken3 (1'b1),
+				.data_a ({16{1'b1}}),
+				.data_b (1'b1),
+				.eccstatus (),
+				.q_b (),
+				.rden_a (1'b1),
+				.rden_b (1'b1),
+				.wren_a (1'b0),
+				.wren_b (1'b0));
+	defparam
+		altsyncram_component.address_aclr_a = "NONE",
+		altsyncram_component.clock_enable_input_a = "BYPASS",
+		altsyncram_component.clock_enable_output_a = "BYPASS",
+		altsyncram_component.init_file = "weights_2.mif",
+		altsyncram_component.intended_device_family = "Cyclone IV E",
+		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
+		altsyncram_component.lpm_type = "altsyncram",
+		altsyncram_component.numwords_a = 784,
+		altsyncram_component.operation_mode = "ROM",
+		altsyncram_component.outdata_aclr_a = "NONE",
+		altsyncram_component.outdata_reg_a = "CLOCK0",
+		altsyncram_component.widthad_a = 10,
+		altsyncram_component.width_a = 16,
+		altsyncram_component.width_byteena_a = 1;
+
 
 endmodule
 
@@ -70,7 +119,7 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 // Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
-// Retrieval info: PRIVATE: MIFfilename STRING "weights.hex"
+// Retrieval info: PRIVATE: MIFfilename STRING "weights_1.mif"
 // Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "784"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -79,13 +128,13 @@ endmodule
 // Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 // Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
 // Retrieval info: PRIVATE: WidthAddr NUMERIC "10"
-// Retrieval info: PRIVATE: WidthData NUMERIC "8"
+// Retrieval info: PRIVATE: WidthData NUMERIC "16"
 // Retrieval info: PRIVATE: rden NUMERIC "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 // Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
-// Retrieval info: CONSTANT: INIT_FILE STRING "weights.hex"
+// Retrieval info: CONSTANT: INIT_FILE STRING "weights_1.mif"
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
@@ -94,18 +143,18 @@ endmodule
 // Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: OUTDATA_REG_A STRING "CLOCK0"
 // Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "10"
-// Retrieval info: CONSTANT: WIDTH_A NUMERIC "8"
+// Retrieval info: CONSTANT: WIDTH_A NUMERIC "16"
 // Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
 // Retrieval info: USED_PORT: address 0 0 10 0 INPUT NODEFVAL "address[9..0]"
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
-// Retrieval info: USED_PORT: q 0 0 8 0 OUTPUT NODEFVAL "q[7..0]"
+// Retrieval info: USED_PORT: q 0 0 16 0 OUTPUT NODEFVAL "q[15..0]"
 // Retrieval info: CONNECT: @address_a 0 0 10 0 address 0 0 10 0
 // Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
-// Retrieval info: CONNECT: q 0 0 8 0 @q_a 0 0 8 0
-// Retrieval info: GEN_FILE: TYPE_NORMAL weight_memory.v TRUE
-// Retrieval info: GEN_FILE: TYPE_NORMAL weight_memory.inc FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL weight_memory.cmp FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL weight_memory.bsf FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL weight_memory_inst.v FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL weight_memory_bb.v TRUE
+// Retrieval info: CONNECT: q 0 0 16 0 @q_a 0 0 16 0
+// Retrieval info: GEN_FILE: TYPE_NORMAL weight_mem2.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL weight_mem2.inc FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL weight_mem2.cmp FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL weight_mem2.bsf FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL weight_mem2_inst.v FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL weight_mem2_bb.v TRUE
 // Retrieval info: LIB_FILE: altera_mf
